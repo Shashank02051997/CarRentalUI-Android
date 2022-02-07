94fa58488de4b8b2d8930cbaacaaa2f6404862c1
Kondja Kapenambili <44747399+kondjaMikaelson@users.noreply.github.com>
2022-02-07 22:46:55 +0200

    more changes

--- app/src/main/java/com/shashank/platform/carrental/MainActivity.java
+++ app/src/main/java/com/shashank/platform/carrental/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //edits
         //edits
+        
+        //need to add application logic here
     }
 }
